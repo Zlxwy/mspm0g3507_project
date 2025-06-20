5da5b555f04ba4a4bec14e972a56dce87423875e
Zlxwy <y3021330020@163.com>
2025-06-20 23:50:43 +0800

    Refactor OLED and Delay Driver Code
    - Added new index files for OLED driver, including OLED.h, OLED_Data.c, and OLED_Data.h, to improve code organization and readability.
    - Implemented delay functions in delay.c and delay.h, enhancing timing control for GPIO operations.
    - Updated main.c to integrate OLED and delay functionalities, ensuring proper initialization and usage.
    - Configured ti_msp_dl_config.c and ti_msp_dl_config.h for system configuration, including GPIO and system control initialization.
    - Improved symbol references and metadata for better code navigation and maintenance.

--- uart_rx_dma_read_jy901s_data/Debug/hardware/oled/subdir_vars.mk
+++ uart_rx_dma_read_jy901s_data/Debug/hardware/oled/subdir_vars.mk
@@ -6,27 +6,27 @@ SHELL = cmd.exe
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../hardware/OLED/OLED.c \
-../hardware/OLED/OLED_Data.c 
+../hardware/oled/OLED.c \
+../hardware/oled/OLED_Data.c 
 
 C_DEPS += \
-./hardware/OLED/OLED.d \
-./hardware/OLED/OLED_Data.d 
+./hardware/oled/OLED.d \
+./hardware/oled/OLED_Data.d 
 
 OBJS += \
-./hardware/OLED/OLED.o \
-./hardware/OLED/OLED_Data.o 
+./hardware/oled/OLED.o \
+./hardware/oled/OLED_Data.o 
 
 OBJS__QUOTED += \
-"hardware\OLED\OLED.o" \
-"hardware\OLED\OLED_Data.o" 
+"hardware\oled\OLED.o" \
+"hardware\oled\OLED_Data.o" 
 
 C_DEPS__QUOTED += \
-"hardware\OLED\OLED.d" \
-"hardware\OLED\OLED_Data.d" 
+"hardware\oled\OLED.d" \
+"hardware\oled\OLED_Data.d" 
 
 C_SRCS__QUOTED += \
-"../hardware/OLED/OLED.c" \
-"../hardware/OLED/OLED_Data.c" 
+"../hardware/oled/OLED.c" \
+"../hardware/oled/OLED_Data.c" 
 
 
